c1d926bdb9b6f7e0c2f8567f58923c957e423370
sahil <sahil13399@gmail.com>
2021-04-12 16:41:31 +0530

    Endpoints updated

--- app.py
+++ app.py
@@ -23,41 +23,17 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/api/dyslexia/upload', methods=['POST'])
+@app.route('/api/dyscalculia/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            response = jsonify({'result': "file Upload failed!!!"})
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-        file = request.files['file']
-        gender = 0 if str(request.form.get('gender')) == "Female" else 1
-        n_language = 0 if str(request.form.get('nlanguage')) == "No" else 1
-        o_language = 0 if str(request.form.get('olanguage')) == "No" else 1
-        age = int(request.form.get('age'))
-        
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            #flash('No selected file')
-            response = jsonify({'result': "file Upload failed!!!"})
-            response.headers.add("Access-Control-Allow-Origin", "*")
-            return response
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            hits,misses,accuracy = handwrittenParser(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        result = dyslexia([gender,n_language,o_language,age,hits,misses,accuracy])
-        response = jsonify({'result': result})
-        response.headers.add("Access-Control-Allow-Origin", "*")
-        return response
+    comp1 = int(request.form.get('comp1'))
+    comp2 = str(request.form.get('comp2'))
+    comp3 = str(request.form.get('comp3'))
+    comp4 = int(request.form.get('comp4'))
             
 
     
 
-@app.route('/api',methods=['POST'])
+@app.route('/api/dyslexia/upload',methods=['POST'])
 def test_func():
     gender = 0 if str(request.form.get('gender')) == "Female" else 1
     n_language = 0 if str(request.form.get('nlanguage')) == "No" else 1
