dd71058bb1cb6f5befb3978518316365d6037bcf
sahil <sahil13399@gmail.com>
2021-04-12 20:00:57 +0530

    Validate unimplemented

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, flash, request, redirect, url_for,jsonify
 from werkzeug.utils import secure_filename
 from model import dyslexia,dyscalculia
 from cloud_vision import handwrittenParser
+from validate import validate_answers
 
 from flask_cors import CORS
 app = Flask(__name__)
@@ -25,13 +26,37 @@ def allowed_file(filename):
 
 @app.route('/api/dyscalculia/upload', methods=['POST'])
 def upload_file():
+    gender = 0 if str(request.form.get('gender')) == "Female" else 1
+    age = int(request.form.get('age'))
     comp1 = int(request.form.get('comp1'))
     comp2 = str(request.form.get('comp2'))
     comp3 = str(request.form.get('comp3'))
     comp4 = int(request.form.get('comp4'))
-            
-
-    
+    count1 = int(request.form.get('count1'))
+    count2 = int(request.form.get('count2'))
+    count3 = int(request.form.get('count3'))
+    count4 = int(request.form.get('count4'))        
+    compute1 = int(request.form.get('compute1'))
+    compute2 = int(request.form.get('compute2'))
+    compute3 = int(request.form.get('compute3'))
+    compute4 = int(request.form.get('compute4'))
+    time1 = str(request.form.get('time1'))
+    time2 = str(request.form.get('time2'))
+    time3 = str(request.form.get('time3'))
+    time4 = str(request.form.get('time4'))
+    geo1 = str(request.form.get('geo1'))
+    geo2 = str(request.form.get('geo2'))
+    geo3 = int(request.form.get('geo3'))
+    geo4 = str(request.form.get('geo4'))
+    l1 = [comp1,comp2,comp3,comp4]
+    l2 = [count1,count2,count3,count4]
+    l3 = [compute1,compute2,compute3,compute4]
+    l4 = [time1,time2,time3,time4]
+    l5 = [geo1,geo2,geo3,geo4]
+    #score1,score2,score3,score4,score5 = validate_answers(l1,l2,l3,l4,l5)
+    #result = dyscalculia([[gender,age,score1,score2,score3,score4,score5]])
+    #return jsonify({'Status':int(result[0])})
+    return jsonify({'Status':geo1})
 
 @app.route('/api/dyslexia/upload',methods=['POST'])
 def test_func():
---
+++ validate.py
@@ -0,0 +1,2 @@
+def validate_answers(l1,l2,l3,l4,l5):
+    pass
